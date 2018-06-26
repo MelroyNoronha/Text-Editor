cd08e7399f977e9b41d97b3653c5bff4c0c6a91a
MelroyNoronha <melmax1208@gmail.com>
2018-06-26 20:38:17 +0530

    First draft of function openTxtFile

--- src/com/editor/Main.java
+++ src/com/editor/Main.java
@@ -1,11 +1,27 @@
 package com.editor;
+import java.io.*;
+import java.nio.file.FileSystem;
 
 public class Main {
     public static void main(String[] args) {
-        openTxtFile("/home/", "test.txt");
+        openTxtFile();
     }
 
-    private static void openTxtFile(String filePath, String fileName){
-        System.out.println(filePath+fileName);
+    private static void openTxtFile(){
+        File testFilePath = new File("/home/mel/Documents/test.txt");
+        String line= null;
+        try{
+            FileReader fileReader = new FileReader(testFilePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            testFilePath + "'");
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
     }
 }
